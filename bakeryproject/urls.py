f7d15a3ab8cbc473a144bbe027c2cafa1bb22e3f
Sangeetha <sangeethasundaran.official@gmail.com>
2026-08-01 15:15:32 +0530

    Initial commit

---
+++ bakeryproject/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for bakeryproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from products import views
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    path('add/', views.add_product,name='add_product'),
+    path('products/', views.product_list, name='product_list'),
+    path('edit/<int:id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:id>/', views.delete_product, name='delete_product'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+
+]
